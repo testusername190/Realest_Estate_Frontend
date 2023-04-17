8d0db7d73bebe59eea4d4cba06f5fb24cf5470a1
VishalSin25 <rvishal251996@gmail.com>
2023-04-17 12:26:15 +0530

    frontend first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Layout from './hocs/Layout';
+import Home from './containers/Home';
+import About from './containers/About';
+import Contact from './containers/Contact';
+import ListingDetail from './containers/ListingDetail';
+import Listings from './containers/Listings';
+import Login from './containers/Login';
+import SignUp from './containers/SignUp';
+import NotFound from './components/NotFound';
+import './sass/main.scss';
+import {Provider} from 'react-redux';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
 
+const App=()=> (
+  <Provider store={store}>
+  <Router>
+      <Layout>
+        <Routes>
+          <Route exact path='/' Component={Home}/>
+          <Route exact path='/about' Component={About}/>
+          <Route exact path='/listings' Component={Listings}/>
+          <Route exact path='/contact' Component={Contact}/>
+          <Route exact path='/listings/:id' Component={ListingDetail}/>
+          <Route exact path='/login' Component={Login}/>
+          <Route exact path='/signup' Component={SignUp}/>
+          {/* <Route Component={NotFound}/> */}
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </Layout>
+  </Router>
+  </Provider>
+);
+ 
 export default App;
